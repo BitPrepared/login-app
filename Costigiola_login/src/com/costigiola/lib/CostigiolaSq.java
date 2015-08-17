616727454d39bcb32b615b255bea81cef0684e89
Nicola Corti <corti.nico@gmail.com>
2015-08-17 16:55:16 +0200

    First commit

---
+++ Costigiola_login/src/com/costigiola/lib/CostigiolaSq.java
@@ -0,0 +1,54 @@
+package com.costigiola.lib;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import costigiola.lib.R;
+
+public class CostigiolaSq extends Activity {
+	ImageView img;
+	String sq;
+	TextView txt;
+	Bundle extras;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seconda);
+        img=(ImageView)this.findViewById(R.id.immagine);
+        txt=(TextView)this.findViewById(R.id.textView1);
+        
+        if (savedInstanceState == null) {
+            extras = getIntent().getExtras();
+            if(extras == null) {
+                sq= null;
+            } else {
+                sq= extras.getString("sq");
+            }
+        } else {
+            sq= (String) savedInstanceState.getSerializable("sq");
+        }
+        
+
+        if (sq.equals(Ragazzi.Bianchi)){
+        	img.setImageResource(R.drawable.rad1);
+        	txt.setText("Sei stato arruolato dalla nave 'Scimmia Pazza', recati in questo posto!");
+        	
+        } else
+	        if (sq.equals(Ragazzi.Rossi)){
+	        	img.setImageResource(R.drawable.rad3);
+	        	txt.setText("Sei stato arruolato dalla nave 'Cetriolo di Mare', recati in questo posto!");
+
+	        } else
+	        	if (sq.equals(Ragazzi.Verdi)){
+		        	img.setImageResource(R.drawable.rad2);
+		        	txt.setText("Sei stato arruolato dalla nave 'Vecchia Signora', recati in questo posto!");
+
+	        	} else
+		        	if (sq.equals(Ragazzi.Gialli)){
+			        	img.setImageResource(R.drawable.rad4);
+			        	txt.setText("Sei stato arruolato dalla nave 'Jolly Rasta', recati in questo posto!");
+
+		        	}
+	}
+}
---
+++ Costigiola_login/src/com/costigiola/lib/Ragazzi.java
@@ -0,0 +1,57 @@
+package com.costigiola.lib;
+
+public class Ragazzi {
+	public static String Rossi="Rossi";
+	public static String Bianchi="Bianchi";
+	public static String Verdi="Verdi";
+	public static String Gialli="Gialli";
+
+	
+	String nome;
+	String cognome;
+	String sq;
+	
+	public Ragazzi (String nome, String cognome, String sq){
+		this.nome=nome;
+		this.cognome=cognome;
+		this.sq =sq;
+	}
+
+	public Ragazzi(String nome, String cognome) {
+		this.nome=nome;
+		this.cognome=cognome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cognome == null) ? 0 : cognome.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ragazzi other = (Ragazzi) obj;
+		if (cognome == null) {
+			if (other.cognome != null)
+				return false;
+		} else if (!cognome.equals(other.cognome))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+}
